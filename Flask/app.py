54849cdd6f99a83ce51f3d56e400f1bf9625d807
~sumitisthename <sumit.d.1996@gmail.com>
2025-07-20 19:54:51 -0500

    POST Method added

--- Flask/app.py
+++ Flask/app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
-def welcome():
+def welcome(methods=['GET']):
     return "<html><h1>Welcome to the Flask application!</h1></html>"
-@app.route('/about')
+
+@app.route('/about',methods=['GET'])
 def about():
     return render_template('index.html')
+
+@app.route('/forms',methods=['GET','POST'])
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f"<html><h1>Form submitted! Hello, {name}!</h1></html>"
+    return render_template('form.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
